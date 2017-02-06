e9f43dce28858f2d3979ae31bab74dd16eb8ef29
trejonh <houset@msoe.edu>
2017-02-06 02:36:41 -0600

    import libs and did hello world tutorial

---
+++ Four_Old_Dudes/Four_Old_Dudes/GameRunner.cs
@@ -0,0 +1,31 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+
+namespace Four_Old_Dudes
+{
+    class GameRunner
+    {
+        static void Main()
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(200, 200), "SFML works!");
+            CircleShape shape = new CircleShape(100f);
+            shape.FillColor = Color.Green;
+            window.SetActive();
+            window.Closed += new EventHandler(OnClosed);
+            while (window.IsOpen)
+            {
+                window.Clear();
+                window.DispatchEvents();
+                window.Draw(shape);
+                window.Display();
+            }
+        }
+
+        static void OnClosed(object sender, EventArgs e)
+        {
+            Window window = (Window)sender;
+            window.Close();
+        }
+    }
+}
